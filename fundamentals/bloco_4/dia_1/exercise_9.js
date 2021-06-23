606a9c7c949e0b312322a548a42ab69431ce3bd2
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-23 20:13:32 -0300

    Exercício 9

---
+++ fundamentals/bloco_4/dia_1/exercise_9.js
@@ -0,0 +1,11 @@
+let num1 = 2;
+let num2 = 4;
+let num3 = 5;
+
+let isOdd = num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0
+
+if (isOdd) {
+    console.log(true);
+} else {
+    console.log(false);
+}
