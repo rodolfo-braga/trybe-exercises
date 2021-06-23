45632d21e11b6b617e743ead4e3d125c33cf0698
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-23 20:10:24 -0300

    Exercício 8

---
+++ fundamentals/bloco_4/dia_1/exercise_8.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 3;
+let num3 = 1;
+
+let isEven = num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0
+
+if (isEven) {
+    console.log(true);
+} else {
+    console.log(false);
+}
