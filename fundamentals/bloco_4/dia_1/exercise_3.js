8bb9e896fcea943692cd753b9c676252c9cd77d9
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-23 16:16:13 -0300

    Exercício 3

---
+++ fundamentals/bloco_4/dia_1/exercise_3.js
@@ -0,0 +1,11 @@
+let var1 = 2;
+let var2 = 4;
+let var3 = 7;
+
+if (var1 > var2 && var1 > var3) {
+    console.log(var1);
+} else if (var2 > var3) {
+    console.log(var2);
+} else {
+    console.log(var3);
+}
