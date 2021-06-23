3c62087a0e4c022430d9727ad23013ea992559d6
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-23 16:00:54 -0300

    Exercício 1

---
+++ fundamentals/bloco_4/dia_1/exercise_1.js
@@ -0,0 +1,8 @@
+let a = 20;
+let b = 10;
+
+console.log(a + b); //Adição
+console.log(a - b); //Subtração
+console.log(a * b); //Multiplicação
+console.log(a / b); //Divisão
+console.log(a % b); //Módulo
