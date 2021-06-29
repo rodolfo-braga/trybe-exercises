15c426eacbb9bd8f3caaae9ec60683e6ea5a1b7b
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-29 12:25:19 -0300

    Bonus - Exercício 1

---
+++ fundamentals/bloco_4/dia_4/exercises_bonus.js
@@ -0,0 +1,36 @@
+//Ex. 1 - Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+function convertRomans(romanNumber) {
+    let romans = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    let sum = romans[romanNumber[romanNumber.length - 1]];
+
+    for (let index = romanNumber.length - 1; index > 0; index -= 1) {
+        if (romans[romanNumber[index - 1]] < romans[romanNumber[index]]) {
+            sum -= romans[romanNumber[index - 1]];
+        } else {
+            sum += romans[romanNumber[index - 1]];
+        }
+    }
+    return sum;
+}
+
+console.log(convertRomans('MCMXCII'));
+
+
+
+
+//Ex. 2 - Crie uma função chamada arrayOfNumbers que receberá a variável vector como parâmetro. Através de um loop for , percorra essa variável, busque os números pares e os adicione a um novo array que deverá ser retornado ao final pela pela função.
+
+
+
+
+
+//Ex. 3 - A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 } quando passado como argumento para a função. Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
