b30702b8c5302b9a3746fe2453105fcec32a3f48
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-28 21:21:10 -0300

    Parte II - Exercício 2

--- fundamentals/bloco_4/dia_4/exercise_part-II.js
+++ fundamentals/bloco_4/dia_4/exercise_part-II.js
@@ -1,4 +1,5 @@
 //Ex. 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
 function verifyPalindrome(word) {
     isPalindrome = false;
     wordBackwards = '';
@@ -11,6 +12,23 @@ function verifyPalindrome(word) {
     return isPalindrome;
 }
 
-console.log(verifyPalindrome('arara'));
-console.log(verifyPalindrome('desenvolvimento'));
+//console.log(verifyPalindrome('arara'));
+//console.log(verifyPalindrome('desenvolvimento'));
 
+
+//Ex. 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function largestNumber() {
+    let indexLargestNumber = 0;
+    let largest = Number();
+    for (let index = 0; index < arguments.length; index += 1) {
+        if (arguments[index] > largest) {
+            largest = arguments[index];
+            indexLargestNumber = index;
+        }
+    }
+    return indexLargestNumber;
+}
+
+let numbers = [2, 3, 6, 7, 10, 1];
+console.log(largestNumber(...numbers));
