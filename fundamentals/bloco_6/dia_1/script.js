c9ebd64b704e9b0bc397e2f3bbe80aab55ce05e5
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-14 20:41:34 -0300

    Exercício 1

---
+++ fundamentals/bloco_6/dia_1/script.js
@@ -0,0 +1,10 @@
+// Adiciona a sigla dos estados na tag select
+const statesInitials = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
+
+let selectState = document.querySelector('#state');
+for (let index = 0; index < statesInitials.length; index += 1) {
+  let state = document.createElement('option');
+  state.value = statesInitials[index];
+  state.innerHTML = statesInitials[index];
+  selectState.appendChild(state);
+}
