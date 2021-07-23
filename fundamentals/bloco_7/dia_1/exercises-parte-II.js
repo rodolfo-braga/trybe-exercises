6d9dc86297e6062c2729ed48a889eaec4cbe6871
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-23 19:37:43 -0300

    Exercício 2 parte II

--- fundamentals/bloco_7/dia_1/exercises-parte-II.js
+++ fundamentals/bloco_7/dia_1/exercises-parte-II.js
@@ -1,3 +1,4 @@
+//Exercício 1
 const factorial = (n) => {
   let result = 1;
   for (let index = 2; index <= n; index += 1) {
@@ -13,3 +14,29 @@ console.log(factorial(5));
 
 const factorial = (n) => n > 1 ? n * factorial(n - 1) : 1;
 console.log(factorial(5));
+
+
+
+
+//Exercício 2
+const longestWord = (text) => {
+  let wordArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+
+  for (const word of wordArray) {
+      if (word.length > maxLength) {
+          maxLength = word.length;
+          result = word;
+      }
+  }
+
+  return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
+
+const longestWord = (text) => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
