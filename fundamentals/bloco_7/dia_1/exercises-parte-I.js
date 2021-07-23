18d727cabbc6f460f378b0d09c67b200d13044f0
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-23 18:44:37 -0300

    Exercício 2 parte I

--- fundamentals/bloco_7/dia_1/exercises-parte-I.js
+++ fundamentals/bloco_7/dia_1/exercises-parte-I.js
@@ -1,3 +1,4 @@
+//Exercício 1
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -10,3 +11,27 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+
+
+
+//Exercício 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+function sortNumbers(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    for (let index2 = 0; index2 < array.length; index2 += 1) {
+      if (array[index] < array[index2]) {
+        let alt = array[index2];
+        array[index2] = array[index];
+        array[index] = alt;
+      }
+    }
+  }
+}
+
+sortNumbers(oddsAndEvens);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
+
+console.log(oddsAndEvens.sort(function(a, b){return a-b}));
