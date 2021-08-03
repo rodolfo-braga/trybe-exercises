7501b7de51044f4d36892d637b8eb4ffce57737d
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-08-03 20:57:08 -0300

    Exercício 2

--- fundamentals/bloco_8/dia_5/exercise2.js
+++ fundamentals/bloco_8/dia_5/exercise2.js
@@ -3,7 +3,7 @@
 
 const assert = require('assert');
 
-// escreva sum abaixo
+const sum = (...elements) => elements.reduce((acc, curr) => acc + curr, 0);
 
 assert.strictEqual(sum(), 0);
 assert.strictEqual(sum(1), 1);
