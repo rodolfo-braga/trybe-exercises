fe5ba92c1995465f60a0f6071d3da2b35bb8e69f
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-08-19 15:13:16 -0300

    Exercício 1

---
+++ fundamentals/bloco_10/dia_2/tests/exercise1.test.js
@@ -0,0 +1,12 @@
+const uppercase = require('../src/exercise1')
+
+test('uppercase "test" to equal "TEST"', (done) => {
+  uppercase('test', (str) => {
+    try {
+      expect(str).toBe('TEST');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
