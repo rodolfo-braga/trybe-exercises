851563043631a5b6f0744846f84af65b8ee7a88a
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-08-19 15:35:04 -0300

    Exercício 3

--- fundamentals/bloco_10/dia_2/tests/getUserName.test.js
+++ fundamentals/bloco_10/dia_2/tests/getUserName.test.js
@@ -14,3 +14,20 @@ describe('getUserName function', () => {
     return expect(actual).rejects.toThrowError(expected);
   })
 })
+
+describe('ASYNC/AWAIT getUserName function', () => {
+  it('returns the name "Mark" when given the id "1"', async () => {
+    const expected = 'Mark';
+    const actual = await getUserName(1);
+
+    expect(actual).toBe(expected);
+  })
+  it('throws an error if the user is not found', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(3);
+    } catch (error) {
+      expect(error.message).toEqual('User with 3 not found.');
+    }
+  })
+})
