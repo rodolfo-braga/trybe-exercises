9246c968bb4059a82ac2e05eec0140814c090286
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-08-19 15:13:35 -0300

    Exercício 2

---
+++ fundamentals/bloco_10/dia_2/tests/getUserName.test.js
@@ -0,0 +1,16 @@
+const getUserName = require('../src/getUserName')
+
+describe('getUserName function', () => {
+  it('returns the name "Mark" when given the id "1"', () => {
+    const expected = 'Mark';
+    const actual = getUserName(1);
+
+    return expect(actual).resolves.toBe(expected);
+  })
+  it('throws an error if the user is not found', () => {
+    const expected = new Error('User with 3 not found.');
+    const actual = getUserName(3);
+
+    return expect(actual).rejects.toThrowError(expected);
+  })
+})
